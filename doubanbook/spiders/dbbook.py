3960452a8af9363e9bb1b9b4441c77ffd5151542
1zehaowang <zehaowang@163.com>
2017-07-16 08:50:10 +0800

    豆瓣九分书爬虫

---
+++ doubanbook/spiders/dbbook.py
@@ -0,0 +1,32 @@
+import scrapy
+import re
+#from ..items import DoubanbookItem
+
+class DbbookSpider(scrapy.Spider):
+    name = "dbbook"
+    start_urls = (
+       'https://www.douban.com/doulist/1264675/',
+    )
+
+    def parse(self, response):
+        #item = DoubanbookItem()
+
+        selector = scrapy.Selector(response)
+        books = selector.xpath('//div[@class="bd doulist-subject"]')
+        for each in books:
+            title = each.xpath('div[@class="title"]/a/text()').extract()[0]
+            rate = each.xpath('div[@class="rating"]/span[@class="rating_nums"]/text()').extract()[0]
+            author = re.search('<div class="abstract">(.*?)<br',each.extract(),re.S).group(1)
+            title = title.replace(' ','').replace('\n','')
+            author = author.replace(' ','').replace('\n','')
+            print title
+            print rate
+            print author + '\n'
+
+            nextPage = selector.xpath('//span[@class="next"]/link/@href').extract()
+            if nextPage:
+                next = nextPage[0]
+                print next
+                yield scrapy.http.Request(next,callback=self.parse)
+
+
